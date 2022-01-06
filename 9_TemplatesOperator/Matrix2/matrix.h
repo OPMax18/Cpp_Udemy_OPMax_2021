b848069c86f66c17a97523409de2b9721938d7d0
OPMax18 <opmax.ges@gmx.de>
2022-01-06 12:30:43 +0100

    update_matrix

--- 9_TemplatesOperator/Matrix2/matrix.h
+++ 9_TemplatesOperator/Matrix2/matrix.h
@@ -1,32 +1,18 @@
 #pragma once
 #include <iostream>
-// template <class T_Matrix>
-// class Matrix;
-template <typename T_Data>
-class Matrix;
-
-template <typename T_Data>
-Matrix<T_Data> operator+(const Matrix<T_Data>& rhs_matrix);
 
 template <typename T_Data>
 class Matrix
 {
 public:
-    Matrix<T_Data>();
-    Matrix<T_Data>(const T_Data& value_1_1, const T_Data& value_2_1, const T_Data& value_1_2, const T_Data& value_2_2);
-    ~Matrix<T_Data>();
+    Matrix();
+    Matrix(const T_Data& value_1_1, const T_Data& value_2_1, const T_Data& value_1_2, const T_Data& value_2_2);
+    ~Matrix();
 
-    friend Matrix<T_Data> operator+<>(const Matrix<T_Data>& rhs_matrix);
-    // friend Matrix<T_Data>& operator+=(const Matrix<T_Data>& lhs_matrix, const Matrix<T_Data>& rhs_matrix)
-    // {
-    //     this->set_1_1(this->get_1_1() + rhs_matrix.get_1_1);
-    //     this->set_2_1(this->get_2_1() + rhs_matrix.get_2_1);
-    //     this->set_1_2(this->get_1_2() + rhs_matrix.get_1_2);
-    //     this->set_2_2(this->get_2_2() + rhs_matrix.get_2_2);
-    //     return *this;
-    // }
-    // friend Matrix<T_Data> operator-<>(const Matrix<T_Data>& rhs_matrix);
-    // friend Matrix<T_Data>& operator-=<>(const Matrix<T_Data>& rhs_matrix);
+    Matrix<T_Data> operator+(const Matrix<T_Data>& rhs_matrix);
+    Matrix<T_Data>& operator+=(const Matrix<T_Data>& rhs_matrix);
+    Matrix<T_Data> operator-(const Matrix<T_Data>& rhs_matrix);
+    Matrix<T_Data>& operator-=(const Matrix<T_Data>& rhs_matrix);
 
     void print_matrix() const;
 
@@ -66,52 +52,46 @@ Matrix<T_Data>::~Matrix()
 }
 
 template <typename T_Data>
-Matrix<T_Data> Matrix<T_Data>::operator+<>(const Matrix<T_Data>& rhs_matrix)
+Matrix<T_Data> Matrix<T_Data>::operator+(const Matrix<T_Data>& rhs_matrix)
 {
+    Matrix matrix;
+    matrix.set_1_1(this->get_1_1() + rhs_matrix.get_1_1());
+    matrix.set_2_1(this->get_2_1() + rhs_matrix.get_2_1());
+    matrix.set_1_2(this->get_1_2() + rhs_matrix.get_1_2());
+    matrix.set_2_2(this->get_2_2() + rhs_matrix.get_2_2());
+    return matrix;
 }
-// Matrix<T_Data> Matrix<T_Data>::operator+(const Matrix<T_Data>& rhs_matrix)
-// {
-//     Matrix matrix;
-//     matrix.set_1_1(this->get_1_1() + rhs_matrix.get_1_1);
-//     matrix.set_2_1(this->get_2_1() + rhs_matrix.get_2_1);
-//     matrix.set_1_2(this->get_1_2() + rhs_matrix.get_1_2);
-//     matrix.set_2_2(this->get_2_2() + rhs_matrix.get_2_2);
-//     return matrix;
-// }
 
-// template <class T_Matrix>
-// template <typename T_Data>
-// Matrix<T_Matrix>& Matrix<T_Matrix>::operator+=(const Matrix<T_Data>& rhs_matrix)
-// {
-//     this->set_1_1(this->get_1_1() + rhs_matrix.get_1_1);
-//     this->set_2_1(this->get_2_1() + rhs_matrix.get_2_1);
-//     this->set_1_2(this->get_1_2() + rhs_matrix.get_1_2);
-//     this->set_2_2(this->get_2_2() + rhs_matrix.get_2_2);
-//     return *this;
-// }
+template <typename T_Data>
+Matrix<T_Data>& Matrix<T_Data>::operator+=(const Matrix<T_Data>& rhs_matrix)
+{
+    this->set_1_1(this->get_1_1() + rhs_matrix.get_1_1());
+    this->set_2_1(this->get_2_1() + rhs_matrix.get_2_1());
+    this->set_1_2(this->get_1_2() + rhs_matrix.get_1_2());
+    this->set_2_2(this->get_2_2() + rhs_matrix.get_2_2());
+    return *this;
+}
 
-// template <class T_Matrix>
-// template <typename T_Data>
-// Matrix<T_Matrix> Matrix<T_Matrix>::operator-(const Matrix<T_Data>& rhs_matrix)
-// {
-//     Matrix matrix;
-//     matrix.set_1_1(this->get_1_1() - rhs_matrix.get_1_1);
-//     matrix.set_2_1(this->get_2_1() - rhs_matrix.get_2_1);
-//     matrix.set_1_2(this->get_1_2() - rhs_matrix.get_1_2);
-//     matrix.set_2_2(this->get_2_2() - rhs_matrix.get_2_2);
-//     return matrix;
-// }
+template <typename T_Data>
+Matrix<T_Data> Matrix<T_Data>::operator-(const Matrix<T_Data>& rhs_matrix)
+{
+    Matrix matrix;
+    matrix.set_1_1(this->get_1_1() - rhs_matrix.get_1_1());
+    matrix.set_2_1(this->get_2_1() - rhs_matrix.get_2_1());
+    matrix.set_1_2(this->get_1_2() - rhs_matrix.get_1_2());
+    matrix.set_2_2(this->get_2_2() - rhs_matrix.get_2_2());
+    return matrix;
+}
 
-// template <class T_Matrix>
-// template <typename T_Data>
-// Matrix<T_Matrix>& Matrix<T_Matrix>::operator-=(const Matrix<T_Data>& rhs_matrix)
-// {
-//     this->set_1_1(this->get_1_1() - rhs_matrix.get_1_1);
-//     this->set_2_1(this->get_2_1() - rhs_matrix.get_2_1);
-//     this->set_1_2(this->get_1_2() - rhs_matrix.get_1_2);
-//     this->set_2_2(this->get_2_2() - rhs_matrix.get_2_2);
-//     return *this;
-// }
+template <typename T_Data>
+Matrix<T_Data>& Matrix<T_Data>::operator-=(const Matrix<T_Data>& rhs_matrix)
+{
+    this->set_1_1(this->get_1_1() - rhs_matrix.get_1_1());
+    this->set_2_1(this->get_2_1() - rhs_matrix.get_2_1());
+    this->set_1_2(this->get_1_2() - rhs_matrix.get_1_2());
+    this->set_2_2(this->get_2_2() - rhs_matrix.get_2_2());
+    return *this;
+}
 
 template <typename T_Data>
 void Matrix<T_Data>::print_matrix() const
